fb48135040e26cc5f2cd895f5ce0ca847e772cbd
Ali Sadeghi <ali909392@gmail.com>
2025-12-26 21:37:03 +0330

    feat: Initialize React app with basic structure and styling
    - Added logo512.png for application branding.
    - Created manifest.json for PWA support with app metadata.
    - Added robots.txt to manage web crawler access.
    - Implemented main App component to fetch and display characters from the Rick and Morty API.
    - Styled application with index.css for a cohesive look and feel.
    - Set up index.js to render the App component within the React application.

---
+++ react-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        console.log("App component has mounted");
+
+        fetch("https://rickandmortyapi.com/api/character")
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                console.log("Fetched data:", data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+
+            <div className="card-container">
+                {data ? (
+                    data.results.map((character) => (
+                        <div key={character.id} className="card">
+                            <h2>{character.name}</h2>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                    ))
+                ) : (
+                    <p>Loading data...</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default App;
